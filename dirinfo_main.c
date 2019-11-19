c70186ce1324080be14d53819a88880373f87062
EShau <EShau00@stuy.edu>
2019-11-19 01:35:06 -0500

    yay

--- dirinfo_main.c
+++ dirinfo_main.c
@@ -5,17 +5,14 @@
 #include <errno.h>
 #include <math.h>
 
-int findSize(char * dir_name, struct dirent * entry);
+int findSize(struct dirent * entry);
 void makeGoodSize(unsigned long size, char print_statement[100]);
-void printSize(char * dir_name);
-void printDirectories(char * dir_name);
-void printFiles(char * dir_name);
+void printSize(char dir_name[100]);
+void printDirectories(char dir_name[100]);
+void printFiles(char dir_name[100]);
 
-int findSize(char * parent_name, struct dirent * entry){
-  if (entry->d_type == 4){
-    return 4096;
-  }
-  else if (entry->d_type == 8){
+int findSize(struct dirent * entry){
+  if (entry->d_type == DT_REG){
     struct stat buffer;
     int file = stat(entry->d_name, &buffer);
     return buffer.st_size;
@@ -43,12 +40,12 @@ void makeGoodSize(unsigned long size, char print_statement[100]){
   }
 }
 
-void printSize(char * dir_name){
+void printSize(char dir_name[100]){
   DIR * directory = opendir(dir_name);
   struct dirent * entry = readdir(directory);
   unsigned long size = 0;
   while (entry){
-    size += findSize(NULL, entry);
+    size += findSize(entry);
     entry = readdir(directory);
   }
   closedir(directory);
@@ -57,12 +54,12 @@ void printSize(char * dir_name){
   printf("Total Diectory Size: %s\n", print_statement);
 }
 
-void printDirectories(char * dir_name){
+void printDirectories(char dir_name[100]){
   printf("Directories: \n");
   DIR * directory = opendir(dir_name);
   struct dirent * entry = readdir(directory);
   while (entry){
-    if (entry->d_type == 4){
+    if (entry->d_type == DT_DIR){
       printf("\t%s\n", entry->d_name);
     }
     entry = readdir(directory);
@@ -70,12 +67,12 @@ void printDirectories(char * dir_name){
   closedir(directory);
 }
 
-void printFiles(char * dir_name){
+void printFiles(char dir_name[100]){
   printf("Files: \n");
   DIR * directory = opendir(dir_name);
   struct dirent * entry = readdir(directory);
   while (entry){
-    if (entry->d_type == 8){
+    if (entry->d_type == DT_REG){
       printf("\t%s\n", entry->d_name);
     }
     entry = readdir(directory);
@@ -83,15 +80,37 @@ void printFiles(char * dir_name){
   closedir(directory);
 }
 
-int main(){
-  char * dir_name = ".";
+int main(int argc, char *argv[]){
+  char dir_name[100];
+  if (argc == 2){
+    strcpy(dir_name, argv[1]);
+  }
+  else{
+    if (argc > 2){
+      printf("Too many arguments supplied.\n");
+    }
+    else{
+      printf("At least one argument expected.\n");
+    }
+    printf("Anyways, using fgets() to find a directory to scan.\n");
+    printf("Enter valid directory name to scan:\n");
+    fgets(dir_name, sizeof(dir_name), stdin);
+    int i = 0;
+    while (dir_name[i] != '\0'){
+      if (dir_name[i] == '\n'){
+        dir_name[i] = '\0';
+        break;
+      }
+      i++;
+    }
+  }
   printf("Statistics for directory: %s\n", dir_name);
   DIR * directory = opendir(dir_name);
   if (directory){
+    closedir(directory);
     printSize(dir_name);
     printDirectories(dir_name);
     printFiles(dir_name);
-    closedir(directory);
   }
   else{
     printf("Failure in opening directory: %s\n", strerror(errno));
